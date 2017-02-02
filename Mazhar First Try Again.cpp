a95db5e60ab8ab3c910e23deb166dbbce6d27635
mightymazhar <msyed9784@gmail.com>
2017-02-02 14:53:57 +0600

    Add files via upload

---
+++ Mazhar First Try Again.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+    int main ()
+    {
+
+    cout<<"Hello World!"<<endl;
+
+   //Write code that says : What is your name?";endl;
+   cout<<"What is your name?";
+   //please enter your name: ";endl;
+   cout<<"\nPlease Enter Your Name: ";
+
+   string your_name_variable;
+
+   cin>>your_name_variable;
+
+   cout<<"Hello there " <<your_name_variable<<"!"<<endl;
+   //"Hello - yourname "
+
+
+    return 0;
+    }
+
