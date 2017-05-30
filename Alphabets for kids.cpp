d17f80d5f9f2d629432ff48c40892f40f6462de6
mightymazhar <msyed9784@gmail.com>
2017-05-30 09:55:44 +0600

    Add files via upload

---
+++ Alphabets for kids.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char choice;
+
+    cout << "Enter a letter" << endl;
+    cin >> choice;
+
+    if (choice == 'A' || choice == 'a')
+    {
+        cout << " A for apple " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else if (choice == 'B' || choice == 'b')
+    {
+        cout << " B for ball " << endl;
+    }
+    else;
+    {
+        cout << "Thank You" << endl;
+    }
+
+    return 0;
+}
